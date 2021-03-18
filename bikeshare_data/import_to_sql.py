2a7106f5ed2c9d59d38ffb3e34d5248e9d11324e
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2021-03-18 16:01:56 -0400

    update imports

--- bikeshare_data/import_to_sql.py
+++ bikeshare_data/import_to_sql.py
@@ -1,48 +1,11 @@
 from pathlib import Path
-import psycopg2
-import sqlalchemy
-from pandas import DataFrame, read_csv, concat
-from datetime import datetime
+import pandas as pd
 
-DB_URI = "postgresql://aaron:password@localhost:5432/indego"
-DATA_FOLDER = Path("/Volumes/GoogleDrive/My Drive/projects/bikeshare-data")
+from bikeshare_data import DEFAULT_DATA_FOLDER
+from bikeshare_data.helpers import _import_df, _execute_query
 
 
-def _execute_query(sql: str, uri: str = DB_URI) -> None:
-    """
-    Use psycopg2 to execute + commit an arbitrary SQL query in the database
-    """
-
-    connection = psycopg2.connect(uri)
-    cursor = connection.cursor()
-
-    cursor.execute(sql)
-
-    cursor.close()
-    connection.commit()
-    connection.close()
-
-
-def _import_df(df: DataFrame, sql_tablename: str, if_exists: str, uri: str = DB_URI) -> None:
-    """
-    Use pandas and sqlalchemy to import a CSV file into a SQL table
-    """
-
-    # Ensure column names are all lower-case
-    df.columns = [x.lower() for x in df.columns]
-
-    print("Importing dataframe with", df.shape[0], "rows to", sql_tablename)
-    print("\t -> Starting at", datetime.now())
-
-    engine = sqlalchemy.create_engine(uri)
-
-    df.to_sql(sql_tablename, engine, if_exists=if_exists)
-
-    engine.dispose()
-    print("\t -> Ended at", datetime.now())
-
-
-def csv_to_postgres(data_folder: Path = DATA_FOLDER) -> None:
+def csv_to_postgres(data_folder: Path = DEFAULT_DATA_FOLDER) -> None:
     """
     Import all CSV files within data_folder to postgres
     """
@@ -51,23 +14,89 @@ def csv_to_postgres(data_folder: Path = DATA_FOLDER) -> None:
 
     for f in data_folder.glob("*.csv"):
         if f.name == "stations.csv":
-            df = read_csv(f)
+            df = pd.read_csv(f)
             _import_df(df, "stations", if_exists="replace")
         else:
             print("Reading", f.name)
-            df = read_csv(f)
+            df = pd.read_csv(f)
             trip_dfs.append(df)
 
-    all_trips_df = concat(trip_dfs, ignore_index=True)
-    _import_df(all_trips_df, "trips", if_exists="replace")
+    all_trips_df = pd.concat(trip_dfs, ignore_index=True)
+    _import_df(all_trips_df, "raw_trips", if_exists="replace")
 
 
 def sql_manipulations():
-    _execute_query("CREATE EXTENSION IF NOT EXISTS postgis;")
+
+    queries = [
+        # enable PostGIS
+        """
+            CREATE EXTENSION IF NOT EXISTS postgis;
+        """,
+        # Start Station ID column name changed over time...
+        # ... this gets it all into one column
+        """
+            UPDATE trips 
+            SET start_station_id = start_station
+            WHERE start_station_id IS NULL;
+        """,
+        # Same for End Station ID column
+        """
+            UPDATE trips 
+            SET end_station_id = end_station
+            WHERE end_station_id IS NULL;
+        """,
+        # Add geom columns for start and end of trip
+        """
+            ALTER TABLE trips
+            ADD COLUMN start_geom geometry(Point, 4326);
+        """,
+        """
+            ALTER TABLE trips
+            ADD COLUMN end_geom geometry(Point, 4326);
+        """,
+        # Update geom columns to hold a point for start/end
+        """
+            UPDATE trips
+            SET start_geom = 
+            ST_SetSRID(
+                ST_MakePoint(
+                    start_lon::float,
+                    start_lat::float
+                ),
+                4326
+            )
+            WHERE start_station_id > 3000 AND start_station_id < 9000;
+        """,
+        """
+            UPDATE trips
+            SET end_geom = 
+            ST_SetSRID(
+                ST_MakePoint(
+                    end_lon::float,
+                    end_lat::float
+                ),
+                4326
+            )
+            WHERE end_station_id > 3000 AND end_station_id < 9000;
+        """,
+        # Create a view with trip records that have start and end geoms...
+        # ... since not all of them have usable lat/lon on both trip ends
+        """
+            DROP VIEW IF EXISTS trips_w_geom;
+            CREATE VIEW trips_w_geom AS
+                SELECT *
+                FROM trips
+                WHERE start_geom IS NOT NULL
+                    AND end_geom IS NOT NULL
+        """,
+    ]
+    for q in queries:
+        _execute_query(q)
 
 
 def main():
     csv_to_postgres()
+    sql_manipulations()
 
 
 if __name__ == "__main__":
