2ca5ff1bfb83def3bc7ab4f5e828105521bc6401
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2021-03-18 16:02:10 -0400

    add defaults

---
+++ bikeshare_data/__init__.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+DEFAULT_DB_URI = "postgresql://postgres:password@localhost:5432/indego"
+DEFAULT_DATA_FOLDER = Path("/Volumes/GoogleDrive/My Drive/projects/bikeshare-data")
+DEFAULT_PROJECT_BOUNDARY_SHP = Path(
+    "/Volumes/GoogleDrive/Shared drives/U_City_FY_21/Data/GIS/Draft_Study_Area_Extent/U_CIty_Study_Area_Dissolve_2.shp"
+)
